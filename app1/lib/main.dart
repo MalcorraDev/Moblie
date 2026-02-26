a79dc42f4f04743a23a55983527c66f564d2a0a7
MalcorraDev <soaresmalcorrajoaovitor@gmail.com>
2026-02-26 20:25:41 -0300

    MoblieAula1Arrumado

---
+++ app1/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("App 1"), backgroundColor: Colors.black),
+        body: Center(child: Text('Hello World!')),
+      ),
+    );
+  }
+}
