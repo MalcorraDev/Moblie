bff65bcf36142ab2e6a0d6c79a908371952f1709
MalcorraDev <soaresmalcorrajoaovitor@gmail.com>
2026-03-04 21:06:53 -0300

    Aprendendo Sintaxes

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Pagina());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Pagina extends StatelessWidget {
+  const Pagina({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("App 1"), backgroundColor: Colors.black),
-        body: Center(child: Text('Hello World!')),
+        appBar: AppBar(
+          title: const Text("Fala pai Titulo"),
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.black,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            margin: EdgeInsets.all(50),
+            decoration: BoxDecoration(border: Border.all()),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Pai Pai",
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+
+                ElevatedButton(onPressed: () {
+                  print("Ola mundo")
+                }, child: Text("Baixar")),
+                Row(children: [Text("FIlho 1"), Text("Filho 2")]),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
